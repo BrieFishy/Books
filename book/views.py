174335153be9c8bd5de5931233e4b469e5e13992
BrieFishy <112446655+BrieFishyW@users.noreply.github.com>
2023-11-06 11:05:15 -0700

    finished

--- book/views.py
+++ book/views.py
@@ -9,7 +9,7 @@ def book_list(request):
     book_name = request.GET.get('book_name')
     if book_name != '' and book_name is not None:
         books_list = books_list.filter(book_name__icontains=book_name)
-    paginator = Paginator(books_list, 3)
+    paginator = Paginator(books_list, 5)
     page = request.GET.get('page')
     books_list = paginator.get_page(page)
     return render(request, 'book.html', {'book_list': books_list})
